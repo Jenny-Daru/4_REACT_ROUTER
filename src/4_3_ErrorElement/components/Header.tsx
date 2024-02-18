d0845f895e7d81c1b6760a9865bcc8bc0fc4b167
jenny <dooly3392@gmail.com>
2024-02-18 21:19:44 +0900

    useNavigate 사용법

--- src/4_3_ErrorElement/components/Header.tsx
+++ src/4_3_ErrorElement/components/Header.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    // 4_4_useNavigate
+    // 유저가 로그인해서 redirect 시키고 싶거나 
+    // 유저가 어떤 페이지로 갔는데 접근 권한이 없거나 했을때 사용 
+    const navigate = useNavigate();
+    const onAboutClick = () => {
+        navigate("/about");
+    }
     return (
         <header>
             <ul>
                 <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/about"}>About</Link></li>
+                <button onClick={onAboutClick}>About</button>
             </ul>
         </header>
     );
